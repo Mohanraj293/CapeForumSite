e7579f03c4c5d05d2eed5999074377a799a48432
Mohanraj293 <mohanrajrengarajr@gmail.com>
2020-08-24 01:28:22 +0530

    Initial commit

---
+++ src/app/news-letter/news-letter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news',
+  templateUrl: './news-letter.component.html',
+  styleUrls: ['./news-letter.component.scss']
+})
+
+export class NewsLetterComponent { }
