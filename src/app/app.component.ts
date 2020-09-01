7cf8967ff7530bf135b8b979072023a625b7fe26
Mohanraj293 <mohanrajrengarajr@gmail.com>
2020-09-01 14:26:33 +0530

    Gallery updates and some minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'CapeForumTrustSite';
+  onActivate(event) {
+    window.scroll(0, 0);
+  }
 }
